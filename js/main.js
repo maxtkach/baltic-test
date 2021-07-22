62b47df1eda5aab68752434f5b4abf22e238aae7
Max Tkach <58748870+maxtkach@users.noreply.github.com>
2021-07-22 17:07:38 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -265,6 +265,18 @@
 		accordion();
 		sliderMain();
 	});
+const anchors = document.querySelectorAll('a[href*="#"]')
 
+for (let anchor of anchors) {
+	anchor.addEventListener("click", function(event){
+		event.preventDefault();
+		const blockID = anchor.getAttribute('href')
+		document.querySelector('' + blockID).scrollIntoView({
+			behavior:"smooth",
+			block:"start"
+		})
+	})
+	
+}
 
 }());
